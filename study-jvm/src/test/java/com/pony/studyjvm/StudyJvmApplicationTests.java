305ea8e6a99bb149bb17ec86f1defa7e610c0cf4
heiPony <1083365468@qq.com>
2020-05-21 11:41:21 +0800

    1

---
+++ study-jvm/src/test/java/com/pony/studyjvm/StudyJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pony.studyjvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
