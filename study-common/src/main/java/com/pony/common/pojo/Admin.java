66fea8d771ae6b1b2c76e79a4bc35e99c490b624
heiPony <1083365468@qq.com>
2020-05-22 10:47:00 +0800

    1

---
+++ study-common/src/main/java/com/pony/common/pojo/Admin.java
@@ -0,0 +1,47 @@
+package com.pony.common.pojo;
+
+public class Admin {
+    private Long id;
+    private String name;
+    private String account;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
--- study-concurrent/src/main/java/com/pony/concurrent/test/Test.java
+++ study-concurrent/src/main/java/com/pony/concurrent/test/Test.java
@@ -1,17 +1,13 @@
 package com.pony.concurrent.test;
 
-import java.math.BigDecimal;
+import com.pony.common.pojo.Admin;
+
 
 public class Test {
     public static void main(String[] args) {
-        BigDecimal a = new BigDecimal("0.4");
-        BigDecimal b = new BigDecimal("0.5");
-        BigDecimal c = new BigDecimal("0.6");
-        for (int i = 0; i <7 ; i++) {
-            System.out.print(a.setScale(0, i).intValue()+",");
-            System.out.print(b.setScale(0, i).intValue()+",");
-            System.out.println(c.setScale(0, i).intValue());
-        }
+        Admin admin = new Admin();
+        admin.setName("pony");
+        System.out.println(admin);
 
     }
 }
